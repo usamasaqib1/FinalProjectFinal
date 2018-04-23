df07bda9934dc566c2e30dbc2192c969daf5fc79
azizahmedkhankkz <azizahmedkhankkz1@gmail.com>
2018-04-23 15:18:34 +0500

    fab removed

--- app/src/main/java/com/example/xubii/finalprojectfinal/StartScreen.java
+++ app/src/main/java/com/example/xubii/finalprojectfinal/StartScreen.java
@@ -28,15 +28,7 @@ public class StartScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        fab.hide();
+
         offlineUser   = new Intent(this, userOffline.class);
 
 
